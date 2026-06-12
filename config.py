cc2223d6b8d4c10a90c9b3d9448981fa209998ab
Aligolver <vrauwdeuntlia@gmail.com>
2026-06-12 18:52:12 +0200

    update

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ class ModelConfig:
     R_out: float = 7e-3 # outer radius [m]
 
     P_vector: np.ndarray = field(default_factory=lambda: np.array([0.000, 0.000, 0.000, 0.002, 0.000])) # membrane permeability
-    v_ret: float = 1 # velocity of retentate [m/s]
-    v_perm: float = 1e-1 # velocity of permeate [m/s]
+    v_ret: float = 1.0e-3 # velocity of retentate [m/s]
+    v_perm: float = 1.0e-3 # velocity of permeate [m/s]
     d_ax: float = 2.0e-5 # dispersion in the axial direction 
     
     n_z: int = 100 # number of grid points in the axial direction
@@ -44,7 +44,7 @@ class ModelConfig:
         default_factory=lambda: np.array([2.0e-6, 1.5e-6, 1.5e-6, 1.5e-6, 1.5e-6])
     )  # Diffusivity per component [m²/s]
     eps_s: float = 0.4  # Solid holdup (catalyst bed voidage component) [-]
-    mu: float = 1.0e-3  # Gas viscosity [Pa·s]
+    mu: float = 2.0e-5  # Gas viscosity [Pa·s]
     dp: float = 2.0e-3  # Particle diameter [m]
     T: float = 400.0  # Operating temperature [K]
     R: float = 8.314  # Ideal gas constant [J/(mol·K)]
@@ -53,24 +53,25 @@ class ModelConfig:
     p_0: float = 200e5  # Reference Pressure for volume change [Pa]
 
     # Kinetic Parameters: Reaction 1 (CO2 + 3H2 <-> MeOH + H2O) (Ghosh et al.)
-    T_ref: float = 573.15  # Reference temperature [K] (300 °C)
+    T1_ref: float = 573.15  # Reference temperature [K] (300 °C)
     k1_pre: float = 6.9e-4  # Forward rate constant at T_ref [mol/(s·bar²·kg_cat)]
     k1_eq: float = 2.5e-4  # Equilibrium constant at T_ref [bar⁻²]
     Ea_1: float = 35.7e3  # Activation energy [J/mol]
     K_CO2_ref: float = 0.79  # CO2 adsorption constant at T_ref [bar⁻¹]
-    dH_CO2: float = -25.9e3  # Enthalpy of adsorption for CO2 [J/mol]
     K_H2_ref: float = 0.76  # H2 adsorption constant at T_ref [bar⁻¹]
+    dH_CO2: float = -25.9e3  # Enthalpy of adsorption for CO2 [J/mol]
     dH_H2: float = -12.5e3  # Enthalpy of adsorption for H2 [J/mol]
 
     # Kinetic Parameters: Reaction 2 (CO2 + 2MeOH <-> DMC + H2O) (ibrahim et al.)  
-    k2_pre: float = 1.15e-3  # Pre-exponential factor [s⁻¹]
-    k2_eq: float = 44.5e-14  # Equilibrium constant at 400 K [-]
+    T2_ref: float = 298.15
+    k2_pre: float = 0.8  # Pre-exponential factor [s⁻¹]
     k_ads1: float = 9
     k_ads2: float = 109
     Ea_DMC: float = 106e3  # Activation energy [J/mol]
-    dV: float = -3.775e-4  # Reaction volume change [m³/mol]
+    dH_DMC: float = -20e3
+    dG_DMC: float = 31e3
+    
     rho_cat: float = 1500 # Catalyst density [kg/m³]
-    p_stand: float = 20e6
     m_cat: float = 4.6
 
     inlet_concentration: np.ndarray = field(default_factory=lambda: np.array([900, 2700, 0.0, 0.0, 0.0])) # inlet concentration of the components, CO2, H2, CH3OH, H2O, DMC
@@ -78,16 +79,18 @@ class ModelConfig:
     tol: float = 1.0e-6  # tolerance for convergence
     maxfev: int = 30    # maximum number of function evaluations
 
-    def k_eff_r1(self) -> float:
-        return self.k1_pre * np.exp(
-            (self.Ea_1 / self.R) * (1.0 / self.T_ref - 1.0 / self.T))
-    
-    def k_eff_r2(self, P_total_Pa: float | np.ndarray) -> float | np.ndarray:
-        return self.k2_pre * np.exp(
-            (-self.Ea_DMC - self.dV * (P_total_Pa - self.p_0)) / (self.R * self.T))
-    
+
     def K_ads(self, K_ref: float, dH: float) -> float:
         return K_ref * np.exp((-dH / self.R) * (1.0 / self.T_ref - 1.0 / self.T))
+
+    def k_eff_r1(self):
+        return self.k1_pre * np.exp(
+            (self.Ea_1 / self.R) * (1.0 / self.T_ref - 1.0 / self.T))
+
+    def k_eq_r2(self):
+        ln_Keq = (-self.dG_DMC / (self.R * self.T2_ref)) + \
+                 (self.dH_DMC / (self.R * self.T2_ref)) * (1.0 - (self.T2_ref / self.T))
+        return float(np.exp(ln_Keq))
 
     @property # external paricle surface area per unit bed volume
     def external_area(self):
